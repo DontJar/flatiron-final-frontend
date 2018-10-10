185ca0816959128ad34296f03dde642ea20b6ac5
github username <github email address>
2018-10-10 15:04:49 -0400

    first commit of front end

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    console.log("App.js is 🚀");
+    return <div>Oh, hello.</div>;
+  }
+}
+
+export default App;
